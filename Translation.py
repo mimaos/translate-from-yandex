2e2f50639e899a4789c1b33f7c1cd2d8e96f2632
mimaos <54391324+mimaos@users.noreply.github.com>
2019-09-12 21:32:32 +0300

    Update Translation.py

--- Translation.py
+++ Translation.py
@@ -12,20 +12,26 @@ from io import StringIO
 
 app = Flask(__name__)
 
-@app.route('/translateenglish', methods=['GET'])
+@app.route('/translateenglish', methods=['POST'])
 def translateenglish():
-    if request.method == 'GET':
-        text = request.form['text']
-
+    if request.method == 'POST':
+        data = request.get_json()
+        text = data['text']
+        print(text)
         params = {
         'key':'trnsl.1.1.20190902T115551Z.906ad44dda89b9c9.7f34373544729af9424560734269a2f63f949a8f',
         'lang':'en-ru',
         'text':text
         }
-
-        r = requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate', params=params)
+        
+        r = requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate', params)
 
         io = StringIO(r.text)
-        result = json.load(io)
-        return result
+        result_yandex = json.load(io)
+        result_text = result_yandex['text']
 
+        result = {
+            'text':result_text
+            }
+       
+        return result
