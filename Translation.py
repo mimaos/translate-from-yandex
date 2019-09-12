d532b9fed906ff89646e0d39ad7b3ea8d7f3cb1d
mimaos <54391324+mimaos@users.noreply.github.com>
2019-09-12 22:01:50 +0300

    Update Translation.py

--- Translation.py
+++ Translation.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 from flask import Flask
 from flask import request
 import requests
@@ -17,7 +14,7 @@ def translateenglish():
     if request.method == 'POST':
         data = request.get_json()
         text = data['text']
-        print(text)
+
         params = {
         'key':'trnsl.1.1.20190902T115551Z.906ad44dda89b9c9.7f34373544729af9424560734269a2f63f949a8f',
         'lang':'en-ru',
